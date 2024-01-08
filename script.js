fc4d3dd6acf2563419b07015a5d075c0cc184acf
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2024-01-08 12:31:35 +0600

    destructuring array

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const restaurant = {
+    name: 'Carbon',
+    location: 'Pahartoli, Raozan',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+};
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
